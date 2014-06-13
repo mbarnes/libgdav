82dccf1568b932ee87e6d43c02a23adc3d68648b
Matthew Barnes <mbarnes@redhat.com>
2014-06-13 12:24:53 -0400

    Add GDavScheduleInboxUrlProperty.

---
+++ libgdav/gdav-schedule-inbox-url-property.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2014 Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the licence, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Matthew Barnes <mbarnes@redhat.com>
+ */
+
+#include "config.h"
+
+#include "gdav-schedule-inbox-url-property.h"
+
+G_DEFINE_TYPE (
+	GDavScheduleInboxUrlProperty,
+	gdav_schedule_inbox_url_property,
+	GDAV_TYPE_HREF_PROPERTY)
+
+static void
+gdav_schedule_inbox_url_property_class_init (GDavPropertyClass *class)
+{
+	GDavParsableClass *parsable_class;
+
+	parsable_class = GDAV_PARSABLE_CLASS (class);
+	parsable_class->element_name = "schedule-inbox-URL";
+	parsable_class->element_namespace = GDAV_XMLNS_CALDAV;
+}
+
+static void
+gdav_schedule_inbox_url_property_init (GDavProperty *property)
+{
+}
+
---
+++ libgdav/gdav-schedule-inbox-url-property.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2014 Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the licence, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Matthew Barnes <mbarnes@redhat.com>
+ */
+
+#ifndef __GDAV_SCHEDULE_INBOX_URL_PROPERTY_H__
+#define __GDAV_SCHEDULE_INBOX_URL_PROPERTY_H__
+
+#include <libgdav/gdav-href-property.h>
+
+/* Standard GObject macros */
+#define GDAV_TYPE_SCHEDULE_INBOX_URL_PROPERTY \
+	(gdav_schedule_inbox_url_property_get_type ())
+#define GDAV_IS_SCHEDULE_INBOX_URL_PROPERTY(obj) \
+	(G_TYPE_CHECK_INSTANCE_TYPE \
+	((obj), GDAV_TYPE_SCHEDULE_INBOX_URL_PROPERTY))
+
+G_BEGIN_DECLS
+
+typedef GDavHRefProperty GDavScheduleInboxUrlProperty;
+typedef GDavHRefPropertyClass GDavScheduleInboxUrlPropertyClass;
+
+GType		gdav_schedule_inbox_url_property_get_type
+						(void) G_GNUC_CONST;
+
+G_END_DECLS
+
+#endif /* __GDAV_SCHEDULE_INBOX_URL_PROPERTY_H__ */
+
