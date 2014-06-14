c51d5ce6348abd80ef2aa21b4e923e00fae4e723
Matthew Barnes <mbarnes@redhat.com>
2014-06-14 11:15:11 -0400

    GDavPropertyUpdate: Learn to serialize().
    Also reimplement how it collects properties by keeping a queue of
    GDavPropertySets, using "names-only" to determine whether to output
    a <set> or <remove> element.

--- libgdav/gdav-property-update.c
+++ libgdav/gdav-property-update.c
@@ -21,33 +21,17 @@
 
 #include "gdav-property-update.h"
 
+#include "gdav-property-set.h"
+
 #define GDAV_PROPERTY_UPDATE_GET_PRIVATE(obj) \
 	(G_TYPE_INSTANCE_GET_PRIVATE \
 	((obj), GDAV_TYPE_PROPERTY_UPDATE, GDavPropertyUpdatePrivate))
 
 struct _GDavPropertyUpdatePrivate {
-	GQueue instructions;
+	GQueue propsets;
 };
 
-typedef struct {
-	GType remove;
-	GDavProperty *set;
-} GDavInstruction;
-
 G_DEFINE_TYPE (GDavPropertyUpdate, gdav_property_update, GDAV_TYPE_PARSABLE)
-
-static GDavInstruction *
-gdav_instruction_new (void)
-{
-	return g_slice_new0 (GDavInstruction);
-}
-
-static void
-gdav_instruction_free (GDavInstruction *instruction)
-{
-	g_clear_object (&instruction->set);
-	g_slice_free (GDavInstruction, instruction);
-}
 
 static void
 gdav_property_update_dispose (GObject *object)
@@ -57,14 +41,56 @@ gdav_property_update_dispose (GObject *object)
 	priv = GDAV_PROPERTY_UPDATE_GET_PRIVATE (object);
 
 	/* XXX GLib needs g_queue_clear_full() */
-	while (!g_queue_is_empty (&priv->instructions)) {
-		GDavInstruction *instruction;
-		instruction = g_queue_pop_head (&priv->instructions);
-		gdav_instruction_free (instruction);
-	}
+	while (!g_queue_is_empty (&priv->propsets))
+		g_object_unref (g_queue_pop_head (&priv->propsets));
 
 	/* Chain up to parent's dispose() method. */
 	G_OBJECT_CLASS (gdav_property_update_parent_class)->dispose (object);
+}
+
+static gboolean
+gdav_property_update_serialize (GDavParsable *parsable,
+                                GHashTable *namespaces,
+                                xmlDoc *doc,
+                                xmlNode *parent,
+                                GError **error)
+{
+	GDavPropertyUpdatePrivate *priv;
+	GList *list, *link;
+	xmlNs *nsdav;
+	gboolean success = TRUE;
+
+	/* The parent node is the <propertyupdate> element. */
+
+	priv = GDAV_PROPERTY_UPDATE_GET_PRIVATE (parsable);
+
+	nsdav = g_hash_table_lookup (namespaces, GDAV_XMLNS_DAV);
+
+	list = g_queue_peek_head_link (&priv->propsets);
+
+	for (link = list; link != NULL; link = g_list_next (link)) {
+		GDavPropertySet *propset = link->data;
+		xmlNode *node;
+		xmlChar *name;
+
+		/* names-only: TRUE means <remove>, FALSE means <set> */
+
+		if (gdav_property_set_get_names_only (propset))
+			name = BAD_CAST "remove";
+		else
+			name = BAD_CAST "set";
+
+		node = xmlNewTextChild (parent, nsdav, name, NULL);
+
+		success = gdav_parsable_serialize (
+			GDAV_PARSABLE (propset),
+			namespaces, doc, node, error);
+
+		if (!success)
+			break;
+	}
+
+	return success;
 }
 
 static void
@@ -72,25 +98,13 @@ gdav_property_update_collect_types (GDavParsable *parsable,
                                     GHashTable *parsable_types)
 {
 	GDavPropertyUpdatePrivate *priv;
-	GList *list, *link;
 
 	priv = GDAV_PROPERTY_UPDATE_GET_PRIVATE (parsable);
 
-	list = g_queue_peek_head_link (&priv->instructions);
-
-	for (link = list; link != NULL; link = g_list_next (link)) {
-		GDavInstruction *instruction = link->data;
-
-		if (instruction->set != NULL) {
-			gdav_parsable_collect_types (
-				GDAV_PARSABLE (instruction->set),
-				parsable_types);
-		} else {
-			g_hash_table_add (
-				parsable_types,
-				GSIZE_TO_POINTER (instruction->remove));
-		}
-	}
+	g_queue_foreach (
+		&priv->propsets,
+		(GFunc) gdav_parsable_collect_types,
+		parsable_types);
 }
 
 static void
@@ -107,6 +121,7 @@ gdav_property_update_class_init (GDavPropertyUpdateClass *class)
 	parsable_class = GDAV_PARSABLE_CLASS (class);
 	parsable_class->element_name = "propertyupdate";
 	parsable_class->element_namespace = GDAV_XMLNS_DAV;
+	parsable_class->serialize = gdav_property_update_serialize;
 	parsable_class->collect_types = gdav_property_update_collect_types;
 }
 
@@ -126,30 +141,58 @@ void
 gdav_property_update_set (GDavPropertyUpdate *update,
                           GDavProperty *property)
 {
-	GDavInstruction *instruction;
+	GDavPropertySet *propset;
+	gboolean need_new_propset;
 
 	g_return_if_fail (GDAV_IS_PROPERTY_UPDATE (update));
 	g_return_if_fail (GDAV_IS_PROPERTY (property));
 
-	instruction = gdav_instruction_new ();
-	instruction->set = g_object_ref (property);
+	/* Add to the most recent GDavPropertySet, if we can. */
 
-	g_queue_push_tail (&update->priv->instructions, instruction);
+	propset = g_queue_peek_tail (&update->priv->propsets);
+
+	/* names-only: TRUE means <remove>, FALSE means <set> */
+
+	need_new_propset =
+		(propset == NULL) ||
+		gdav_property_set_get_names_only (propset);
+
+	if (need_new_propset) {
+		propset = gdav_property_set_new ();
+		gdav_property_set_set_names_only (propset, FALSE);
+		g_queue_push_tail (&update->priv->propsets, propset);
+	}
+
+	gdav_property_set_add (propset, property);
 }
 
 void
 gdav_property_update_remove (GDavPropertyUpdate *update,
                              GType property_type)
 {
-	GDavInstruction *instruction;
+	GDavPropertySet *propset;
+	gboolean need_new_propset;
 
 	g_return_if_fail (GDAV_IS_PROPERTY_UPDATE (update));
 	g_return_if_fail (!G_TYPE_IS_ABSTRACT (property_type));
 	g_return_if_fail (g_type_is_a (property_type, GDAV_TYPE_PROPERTY));
 
-	instruction = gdav_instruction_new ();
-	instruction->remove = property_type;
+	/* Add to the most recent GDavPropertySet, if we can. */
 
-	g_queue_push_tail (&update->priv->instructions, instruction);
+	propset = g_queue_peek_tail (&update->priv->propsets);
+
+	/* names-only: TRUE means <remove>, FALSE means <set> */
+
+	need_new_propset =
+		(propset == NULL) ||
+		!gdav_property_set_get_names_only (propset);
+
+	if (need_new_propset) {
+		propset = gdav_property_set_new ();
+		gdav_property_set_set_names_only (propset, TRUE);
+		g_queue_push_tail (&update->priv->propsets, propset);
+	}
+
+	gdav_property_set_add_type (propset, property_type);
 }
 
